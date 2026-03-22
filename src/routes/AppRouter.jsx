3187e064c96b28e08aeed16f040a06d519afef65
Breiner Bonilla <bonillabreiner296@gmail.com>
2026-03-21 23:02:05 -0400

    Se actualizó la versión de tailwind v3 a v4
    Se estructuró las carpetas según los módulos que se desarrollarán

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import AppLayout from "../layout/AppLayout.jsx";
-import MemberPage from "../pages/MemberPage.jsx"
+import MemberPage from "../pages/modules/members/MemberPage.jsx";
 
 export default function AppRouter() {
   return (
@@ -8,7 +8,7 @@ export default function AppRouter() {
       <Routes>
         
         <Route element={<AppLayout />}>
-            <Route path="/miembros" element={<MemberPage />} />
+            <Route path="/" element={<MemberPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
